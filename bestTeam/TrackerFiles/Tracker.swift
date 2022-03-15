e755cd01d6fbfae7e9b350c9345d8aaab9df396d
Carina Adrianzen <carinadyann@csu.fullerton.edu>
2022-03-15 08:05:34 -0700

    unit tests and classes

--- bestTeam/TrackerFiles/Tracker.swift
+++ bestTeam/TrackerFiles/Tracker.swift
@@ -9,48 +9,123 @@ import Foundation
 
 class Tracker {
     var currentDay: Int
+    var weekCount: Int
     var weekday: [Int : String] = [:]
+    //var workouts: [String: func] = [:]
     
     init() {
-        currentDay = 1
-        weekday[1] = "Sunday"
-        weekday[2] = "Monday"
-        weekday[3] = "Tuesday"
-        weekday[4] = "Wednesday"
-        weekday[5] = "Thursday"
-        weekday[6] = "Friday"
-        weekday[7] = "Saturday"
+        currentDay = 0
+        weekCount = 0
+        weekday[0] = "Sunday"
+        weekday[1] = "Monday"
+        weekday[2] = "Tuesday"
+        weekday[3] = "Wednesday"
+        weekday[4] = "Thursday"
+        weekday[5] = "Friday"
+        weekday[6] = "Saturday"
     }
     
     init(_ day: Int) {
         currentDay = day
-        weekday[1] = "Sunday"
-        weekday[2] = "Monday"
-        weekday[3] = "Tuesday"
-        weekday[4] = "Wednesday"
-        weekday[5] = "Thursday"
-        weekday[6] = "Friday"
-        weekday[7] = "Saturday"
+        weekCount = 0
+        weekday[0] = "Sunday"
+        weekday[1] = "Monday"
+        weekday[2] = "Tuesday"
+        weekday[3] = "Wednesday"
+        weekday[4] = "Thursday"
+        weekday[5] = "Friday"
+        weekday[6] = "Saturday"
     }
     
     func nextDay() ->String {
-        var index: Int = 1
+        var index: Int = 0
         var dayOfWeek: String = ""
-        if(currentDay < 7) {
+        if(currentDay < 6) {
             index += 1
             dayOfWeek = weekday[index] ?? ""
         } else {
-            index = 1
+            index = 0
+            weekCount += 1
             dayOfWeek = weekday[1] ?? ""
         }
         return dayOfWeek
     }
     
-    func workoutForDay() {
-        
+    func workoutForDay() ->Void { //change return type
+        //monday: chests; tuesday: legs; wednesday: cardio; thursday: back; friday: abs; saturday: arms; sunday: rest day
+        if(currentDay == 0) {
+            //return rest day or suggested workout
+        } else if (currentDay == 1) {
+            //return chests
+        } else if (currentDay == 2) {
+            //return legs
+        } else if (currentDay == 3) {
+            //return cardio
+        } else if (currentDay == 4) {
+            //return back
+        } else if (currentDay == 5) {
+            //return abs
+        } else if (currentDay == 6) {
+            //return arms
+        } else {
+            //return rest day or suggested workout
+        }
+    }
+    
+    func swipeThrough() ->Void { //change return to Int
+//        var index: Int = 0
+//        if(swipe through left) {
+//            index += 1
+//        } else if (swipe through right){
+//            index -= 1
+//        }
+//        return index
+    }
+    
+    func workoutSchedule() ->Void {
+//        for(_,_) in workouts {
+//            if(workouts[swipeThrough()] == "Sunday") {
+//                //return rest day or suggest3ed workout
+//            } else if(workouts[swipeThrough()] == "Monday") {
+//                //return chests workout
+//            } else if(workouts[swipeThrough() == "Tuesday"]) {
+//                //return legs
+//            } else if(workouts[swipeThrough()] == "Wednesday") {
+//                //return cardio
+//            } else if(workouts[swipeThrough() == "Thursday"]) {
+//                //return back
+//            } else if(workouts[swipeThrough() == "Friday"]) {
+//                //return abs
+//            } else if(workouts[swipeThrough() == "Saturday"]) {
+//                //return arms
+//            } else {
+//                //return rest day or suggested workout
+//            }
+//        }
+    }
+    
+    func weekTracker() ->Void {
+        print("Congratulations! You made it through week \(weekCount)!")
+        let randomPhrase = Int.random(in: 1...5)
+        switch randomPhrase {
+        case 1:
+            print("Keep up the good work!")
+        case 2:
+            print("Awesome job!")
+        case 3:
+            print("Doing great!")
+        case 4:
+            print("Looking good!")
+        case 5:
+            print("Great Work!")
+        default:
+            print("Amazing progress!")
+        }
     }
 }
 
+//=======================================================================================
+//First Draft:
 //class Tracker {
 //    var weekDay: String
 //    var sunday: Int
@@ -135,3 +210,4 @@ class Tracker {
 //        return currentDay
 //    }
 //}
+//=======================================================================================
