3041224c9248653e90906fcbe3e9ba330733dec0
chunged4 <edchung482@csu.fullerton.edu>
2022-03-17 08:30:15 -0700

    Initial Commit

---
+++ bestTeam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bestTeam
+//
+//  Created by csuftitan on 3/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
