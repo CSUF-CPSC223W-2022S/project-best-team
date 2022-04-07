b3631d928da38bee2986a962261f5d846e78e7b5
anthonyvences <66196236+anthonyvences@users.noreply.github.com>
2022-04-07 08:18:42 -0700

    view control progress

---
+++ bestTeam/workoutGoal_files/workoutGoal.swift
@@ -0,0 +1,119 @@
+//
+//  workoutGoal.swift
+//  bestTeam
+//
+//  Created by Anthony Vences on 3/10/22.
+//
+import Foundation
+// shoulders, arms, chest, back, abdominals, legs
+
+//shoulders: trapezius (traps) (top of shoulders)
+//arms: biceps (front of upper arms), triceps (back of upper arms), forearms (lower arm), latissimus dorsi (lats) (under the armpits)
+//chest :
+//back: glutes (butt and 
+//abdominals: abdomianls
+//legs : calves(lower leg), hamstrings(back of upper leg), quadriceps (front of upper leg),
+
+// functions: generateWorkout,
+
+
+// muscle
+class workoutGoal {
+    var focusedMuscle: String
+    var specificMuscle: String
+    // Focused Muscles
+    var Muscle1 = "Shoulder"
+    var Muscle2 = "Arms"
+    var Muscle3 = "Chest"
+    var Muscle4 = "Back"
+    var Muscle5 = "Abdominals"
+    var Muscle6 = "Legs"
+    // Types of focused muscles
+    var shoulder : [String] = ["Trapezius"]
+    var arms : [String] = ["Biceps", "Triceps", "Forearms", "Latissimus Dorsi"]
+    var chest : [String] = ["Chest"]
+    var back : [String] = ["Glutes"]
+    var abdominals : [String] = ["Abdominals"]
+    var legs : [String] = ["Calves", "Legs", "Hamstrings", "Quadriceps"]
+    
+    init() {
+        shoulder = []
+        arms = []
+        chest = []
+        back = []
+        abdominals = []
+        legs = []
+    }
+    
+    func generateWorkout1 (focusedMuscle : String) -> String {
+        let specificMuscle : String?
+    if focusedMuscle == Muscle1 {
+       // Ask which specific muscle it would like to workout with button
+        //return specificMuscle
+       }
+       else if focusedMuscle == Muscle2 {
+           // Ask which specific muscle it would like to workout with button
+            //return specificMuscle
+       }
+       else if focusedMuscle == Muscle2 {
+           // Ask which specific muscle it would like to workout with button
+            //return specificMuscle
+       }
+       else if focusedMuscle == Muscle3 {
+           // Ask which specific muscle it would like to workout with button
+            //return specificMuscle
+       }
+       else if focusedMuscle == Muscle4 {
+           // Ask which specific muscle it would like to workout with button
+            //return specificMuscle
+       }
+       else if focusedMuscle == Muscle5 {
+           // Ask which specific muscle it would like to workout with button
+            //return specificMuscle
+       }
+       else if focusedMuscle == Muscle6 {
+           // Ask which specific muscle it would like to workout with button
+            //return specificMuscle
+       }
+            
+            
+        func generateWorkout2 (generateWorkout1: (String)-> String) -> String {
+        if specificMuscle == shoulder[0] {
+    //output schedule
+        }
+//        else if specificMuscle == arms[0]{
+            
+        }
+//        else if specificMuscle == arms[1] {
+            
+        }
+//     else if specificMuscle == arms[2] {
+            
+        }
+//        else if specificMuscle == arms[3] {
+            
+    //  }
+        
+//    }
+    
+        
+
+    
+ 
+    
+    
+        
+        
+        
+        
+        
+  //  }
+//func generateWorkout (focusedMuscle : string, specificMuscle : String
+
+    
+    
+    
+    
+    
+    
+}
