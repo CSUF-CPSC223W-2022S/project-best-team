b0b47602b8f80642e3d7c09cd5d8b6ee5c7b65ff
anthonyvences <66196236+anthonyvences@users.noreply.github.com>
2022-05-05 09:51:19 -0700

    5/5 update

--- bestTeam/workoutGoal_files/workoutGoal.swift
+++ bestTeam/workoutGoal_files/workoutGoal.swift
@@ -36,65 +36,80 @@ class workoutGoal {
     var abdominals : [String] = ["Abdominals"]
     var legs : [String] = ["Calves", "Legs", "Hamstrings", "Quadriceps"]
     
+    var workoutIntro : String = "Workouts you should do:"
+    var trapeziusSchedule : String = ""
+
     init() {
+        focusedMuscle = " "
+        specificMuscle = " "
         shoulder = []
         arms = []
         chest = []
         back = []
         abdominals = []
         legs = []
-    }
-    
-    func generateWorkout1 (focusedMuscle : String) -> String {
-        let specificMuscle : String?
-    if focusedMuscle == Muscle1 {
-       // Ask which specific muscle it would like to workout with button
-        //return specificMuscle
-       }
-       else if focusedMuscle == Muscle2 {
-           // Ask which specific muscle it would like to workout with button
-            //return specificMuscle
-       }
-       else if focusedMuscle == Muscle2 {
-           // Ask which specific muscle it would like to workout with button
-            //return specificMuscle
-       }
-       else if focusedMuscle == Muscle3 {
-           // Ask which specific muscle it would like to workout with button
-            //return specificMuscle
-       }
-       else if focusedMuscle == Muscle4 {
-           // Ask which specific muscle it would like to workout with button
-            //return specificMuscle
-       }
-       else if focusedMuscle == Muscle5 {
-           // Ask which specific muscle it would like to workout with button
-            //return specificMuscle
-       }
-       else if focusedMuscle == Muscle6 {
-           // Ask which specific muscle it would like to workout with button
-            //return specificMuscle
-       }
-            
-            
-        func generateWorkout2 (generateWorkout1: (String)-> String) -> String {
-        if specificMuscle == shoulder[0] {
-    //output schedule
-        }
-//        else if specificMuscle == arms[0]{
-            
-        }
-//        else if specificMuscle == arms[1] {
-            
-        }
-//     else if specificMuscle == arms[2] {
-            
-        }
-//        else if specificMuscle == arms[3] {
-            
-    //  }
         
-//    }
+        
+    }
+
+    func generateWorkout1 (focusedMuscle : String) -> String { // Returns muscle options
+        if focusedMuscle == Muscle1 {
+            let shoulderOptions : String = shoulder[0]
+            return shoulderOptions
+       }
+          else if focusedMuscle == Muscle2 {
+              
+              let armOptions : String = arms[0] + arms [1] + arms [2]
+              return armOptions
+          }
+          else if focusedMuscle == Muscle3 {
+         
+              let chestOptions : String = chest[0]
+              return chestOptions
+         }
+         else if focusedMuscle == Muscle4 {
+          
+             let backOptions : String = back[0] + back [1]
+             return backOptions
+         }
+         else if focusedMuscle == Muscle5 {
+          
+             let abdominalsOptions : String = abdominals[0]
+             return abdominalsOptions
+         }
+         else if focusedMuscle == Muscle6 {
+         
+             let legOptions : String = legs[0] + legs[1] + legs[2] + legs[3]
+             return legOptions
+         }
+        else {
+            return "Error"
+        }
+    }
+
+        func generateWorkout2 (generateWorkout1: (String)-> String) -> String { // returns rough workout schedule
+            
+            if specificMuscle == shoulder[0] {
+            return "workout"
+        }
+        else if specificMuscle == arms[0]{
+             return "Workout"
+        }
+        else if specificMuscle == arms[1] {
+             return "Workout"
+        }
+        else if specificMuscle == arms[2] {
+             return "Workout"
+        }
+        else if specificMuscle == arms[3] {
+             return "Workout"
+        }
+            else {
+                return "Error"
+            }
+        }
+        
+}
     
         
 
@@ -116,4 +131,4 @@ class workoutGoal {
     
     
     
-}
+
